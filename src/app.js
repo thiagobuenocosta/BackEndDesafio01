5bd4590ce7690ecec51026805ae5cad345c37fe4
Thiago Bueno Costa <th.bueno.95@gmail.com>
2020-11-17 05:33:03 -0300

    feat: created, repositories record and repositories list

--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+app.use( express.json() );
+app.use( cors() );
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const likes = 0;
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes
+  };
+  repositories.push(repositorie);
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
